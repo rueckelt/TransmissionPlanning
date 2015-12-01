d11bc1e7751de492bd97cc29ef351a258c633b09
Tobias Rueckelt <tobias.rueckelt@kom.tu-darmstadt.de>
2015-12-01 20:13:11 +0100

    scenario

--- my_logs/longTest1/parameters_log.m
+++ my_logs/longTest1/parameters_log.m
@@ -1,10 +1,10 @@
 
-% 2015-10-17 12:16:08
+% 2015-11-12 23:35:16
 
 % my_logs\longTest1\
-max_time = 4;
-max_flows = 4;
-max_nets = 4;
+max_time = 3;
+max_flows = 6;
+max_nets = 6;
 max_rep = 30;
 
 scheduler_logs= char('optimization_log.m','random_log.m','priority_log.m','priorityMatch_log.m');
--- src/main.java
+++ src/main.java
@@ -9,19 +9,21 @@ public class main {
 
 	
 	public static void main(String[] args) {
-		boolean deco = false;
+		boolean decomp = false;
 //		int n=0;//3_0_2 soll
 //		int f=2;
 //		int t=3;
-		int f=4;
-		int n=4;
-		int t=4;
+		int f=6;
+		int n=6;
+		int t=3;
 		int rep=30;
 		String logpath= "my_logs"+File.separator+"longTest1";
 		
-//		for(int rep = 4; rep<=rep1; rep++){
+//		for(int rep = 0; rep<=30; rep++){
 			EvaluationScenarioCreator eval = new EvaluationScenarioCreator(t,n,f,rep,logpath);
 			eval.evaluateAll();
+//			eval.calculateInstance_t_n_i(t, n, f, rep, logpath, false ,false, decomp);	//recalc
+//
 //			eval.calculateInstance_t_n_i(t, n, f, rep, logpath, false ,true, decomp);	//recalc
 //			eval.calculateInstance_t_n_i(t, n, f, rep, logpath, true ,false, decomp);	//overwrite
 //			System.gc();
