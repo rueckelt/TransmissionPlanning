002b7a714bc44c29f9f21794370aad33d7093d33
Tobias Rückelt <tobias.rueckelt@de.opel.com>
2016-05-13 09:11:30 +0200

    Prapared eval_4_4_3 for wtp 10 Matlab script

--- my_logs/eval_4_4_3_c/parameters_log.m
+++ my_logs/eval_4_4_3_c/parameters_log.m
@@ -1,13 +1,13 @@
 
-% 2016-05-12 17:31:03
+% 2016-05-12 17:55:00
 
-% my_logs/eval_4_4_3_c/
+% my_logs\eval_4_4_3_c\
 max_time = 4;
 max_flows = 4;
 max_nets = 3;
 max_rep = 30;
 evaluate_max_only = 1;
 
-scheduler_logs= {'GreedyOnlineOpp_log.m','GreedyOnline_log.m'};
-schedulers= {'GreedyOnlineOpp','GreedyOnline'};
+scheduler_logs= {'optimization_log.m','Greedy_log.m','GreedyOnlineOpp_log.m','GreedyOnline_log.m','Random_log.m'};
+schedulers= {'Optimization','Greedy','GreedyOnlineOpp','GreedyOnline','Random'};
 
