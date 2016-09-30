0a9176da00e1b5601560b4b865f30980f969794f
Tobias Rueckelt <tobias.rueckelt@kom.tu-darmstadt.de>
2016-09-30 18:21:46 +0200

    results of new rating model greedy scheduler

--- my_logs/eval_4_4_3_c15/parameters_log.m
+++ my_logs/eval_4_4_3_c15/parameters_log.m
@@ -1,14 +1,13 @@
 
-% 2016-05-27 12:25:53
-% 2016-05-27 15:15:46
+% 2016-09-30 18:18:44
 
 % my_logs/eval_4_4_3_c15/
-max_time = 5;
-max_flows = 5;
+max_time = 2;
+max_flows = 4;
 max_nets = 5;
 max_rep = 30;
 evaluate_max_only = 1;
 
-scheduler_logs= {'Optimization_log.m','Greedy_0_log.m','GreedyOnlineOpp_0_log.m','GreedyOnline_log.m','Random_log.m'};
-schedulers= {'Optimization','Greedy_0','GreedyOnlineOpp_0','GreedyOnline','Random'};
+scheduler_logs= {'Greedy_0_H2_log.m','Greedy_0_log.m'};
+schedulers= {'Greedy_0_H2','Greedy_0'};
 
