2c1311551e3bcf8bccd6a665656026bb5240ce0d
Tobias Rückelt <tobias.rueckelt@de.opel.com>
2016-12-14 16:57:15 +0100

    cdf plot matlab added. But must still be modified according to data, line colors..

---
+++ matlab/cdfPrint.m
@@ -0,0 +1,56 @@
+function [ output_args ] = cdfPrint( output_name, my_xlabel, data, index1, prot_names)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    
+    MAX_ELEMENTS=500;
+    [~,n_prot]=size(data);
+    
+    linestyles = {'-','--',':','-.'};
+    linecolors = {'black','magenta','blue','red','green','cyan','yellow'};
+
+    fig=figure;%('visible','off');
+    hold on
+    grid on
+    
+    for prot=1:n_prot
+        if ~isempty(data{index1,prot})
+            %reduce data: otherwise matlab runs into OUT OF MEMORY error
+            %while plotting or creating tiks
+            data_sort=data{index1,prot};
+            data_sort=sort(data_sort);
+            [~,len]=size(data_sort);
+            k=ceil(len/MAX_ELEMENTS);
+ 
+            data_reduced=zeros(1,MAX_ELEMENTS); %init with zeros
+            j=1;
+            for i=1:len
+                if mod(i,k)==0
+                    data_reduced(j)=data_sort(i);
+                    j=j+1;
+                end
+            end
+            data_reduced=data_reduced(data_reduced~=0); %delete zeros
+                
+            ['reduced for ' output_name]
+            h = cdfplot(data_reduced); 
+          %  meanvalue=mean(data{index1, prot});
+          %  line([0.5 0.5], meanvalue);
+            set(h,'LineStyle',linestyles{mod(prot-1,4)+1},...
+                'Color',linecolors{mod(prot-1,7)+1},...
+                'LineWidth',1.25);
+        end
+    end
+    
+   % l=refline(0,0.5);
+%    set(l,'color',[0.9,0.7,0.1]);
+    xlabel(my_xlabel);
+    ylabel([]);%'cumulative distribution');
+    legend(prot_names{1}, prot_names{2}, prot_names{3});
+   % axes1 = axes('Parent',fig,'XScale','log');
+    set(gca,'xscale','log', 'XMinorTick', 'off');
+    title([]);
+    
+    matlab2tikz(output_name,'width','\figW','height','\figH','showInfo',false);
+end
+
+
